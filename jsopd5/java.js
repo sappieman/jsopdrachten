b94dcd46d7d7a6495e0228e9bc436209a22840fd
bas klaichid <basklaichid@gmail.com>
2022-01-25 11:30:29 +0100

    first commit

--- jsopd5/java.js
+++ jsopd5/java.js
@@ -1,67 +1,149 @@
 const myTitle = document.getElementById("myTitle");
 const myImage = document.getElementById("myImage");
-const myInput = document.getElementById("myInput")
+const myInput = document.getElementById("myInput"); 
+
+let directionButtons = {
+    "noord": document.getElementById('knopNoord'),
+    "oost": document.getElementById('knopOost'),
+    "zuid": document.getElementById('knopZuid'),
+    "west": document.getElementById('knopWest')
+}
+
+
+let current_index = 0;
 
 
 let locaties = [
-    {
-        "titel":"plaats 0",
-        "image":"img/0.jpg"
+    {//0 = object
+        "titel":"0 ingang silver bullet",
+        "image":"img/0.jpg",
+        "directions":{
+            "zuid": 1
+        }
+    },
+    {//1 = object
+        "titel":"1 gang bij docentenkamer",
+        "image":"img/1.jpg",
+        "directions":{
+            "noord": 0,
+            "west": 2,
+            "oost": 5,
+            "zuid": 4
+        }
     },
     {
-        "titel":"plaats 1",
-        "image":"img/1.jpg"
+        "titel":"2 gang voor de trap",
+        "image":"img/2.jpg",
+        "directions":{
+            "oost": 1,
+            "zuid": 3
+            
+        }
     },
     {
-        "titel":"plaats 2",
-        "image":"img/2.jpg"
+        "titel":"plaats 33 vergaderkamer",
+        "image":"img/3.jpg",
+        "directions":{
+            "noord": 2
+        }
     },
     {
-        "titel":"plaats 3",
-        "image":"img/3.jpg"
+        "titel":"4 docentenkamer",
+        "image":"img/4.jpg",
+        "directions":{
+            "noord": 1
+        }
     },
     {
-        "titel":"plaats 4",
-        "image":"img/4.jpg"
+        "titel":"5 gang bij lokaal 0.90",
+        "image":"img/5.jpg",
+        "directions":{
+            "noord": 10,
+            "west": 1,
+            "oost": 6
+        }
     },
     {
-        "titel":"plaats 5",
-        "image":"img/5.jpg"
+        "titel":"6 gang bij het toilet",
+        "image":"img/6.jpg",
+        "directions":{
+            "west": 5,
+            "oost": 7,
+            "zuid": 11
+        }
     },
     {
-        "titel":"plaats 6",
-        "image":"img/6.jpg"
+        "titel":"7 gang bij lokaal 0.92 en 0.93",
+        "image":"img/7.jpg",
+        "directions":{
+            "noord": 9,
+            "west": 6,
+            "zuid": 8
+        }
     },
     {
-        "titel":"plaats 7",
-        "image":"img/7.jpg"
+        "titel":"8 klaslokaal 0.93",
+        "image":"img/8.jpg",
+        "directions":{
+            "noord": 7
+        }
     },
     {
-        "titel":"plaats 8",
-        "image":"img/8.jpg"
+        "titel":"9 klaslokaal 0.92",
+        "image":"img/9.jpg",
+        "directions":{
+            "west": 10,
+            "zuid": 7
+        }
     },
     {
-        "titel":"plaats 9",
-        "image":"img/9.jpg"
+        "titel":"10 klaslokaal 0.90",
+        "image":"img/10.jpg",
+        "directions":{
+            "oost": 9,
+            "zuid": 5
+        }
     },
     {
-        "titel":"plaats 10",
-        "image":"img/10.jpg"
-    },
-    {
-        "titel":"plaats 11",
-        "image":"img/11.jpg"
+        "titel":"11 het toilet",
+        "image":"img/11.jpg",
+        "directions":{
+            "noord": 6
+        }
     },
 ];
 
 function show(index){
     myTitle.innerHTML = locaties[index].titel;
     myImage.src = locaties[index].image;
+    current_index = index;
+    //knoppen opnieuw berekenen
+    updateDirections();
 }
-
+function updateDirections(){
+    //haal de mogelijke directions op voor de current_index
+    let possible = locaties[current_index].directions;
+    // zet de direction keys in een aparte variabele
+    let possible_keys = Object.keys(possible);
+    //zet de keys van de buttons in een aparte variable
+    let button_keys = Object.keys(directionButtons);
+    console.log(possible_keys)
+    //zet eerst alle knoppen uit
+    for(const key of button_keys){
+        directionButtons[key].style.visibility = "hidden";
+    }
+    //zet nu de mogelijke knoppen(directions) aan
+    for(const key of possible_keys){
+        directionButtons[key].style.visibility = "visible";
+    }
+}
 function getInput(){
     show(myInput.value)
-    console.log(myInput.value)
     myInput.value = "";
     myInput.focus();
 }
+function goDirection(richting){
+    let punt_index = locaties[current_index].directions[richting];
+    show(punt_index);
+}
+show(0)
